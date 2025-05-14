dc38ba2d522e82280813dfe41ab401f407e6171d
Robiu-Sani <hafazrobiussani@gmail.com>
2025-05-14 22:32:06 +0600

    feture section is done

--- src/app/home/ThirdSection.tsx
+++ src/app/home/ThirdSection.tsx
@@ -1,10 +1,21 @@
+import { FeaturesSection } from "components/ui/FeaturesSection";
 import { TextHoverEffect } from "components/ui/text-hover-effect";
 import React from "react";
 
 export default function ThirdSection() {
   return (
     <div>
-      <TextHoverEffect text="CODE BIRUNY" duration={0.2} />
+      <div className="container md:mt-[-140px] mt-[-60px] px-2 mx-auto">
+        <TextHoverEffect text="CODE BIRUNY" duration={0.2} />
+      </div>
+      <div className="container px-2 md:mt-[-120px] pt-[30px] mx-auto">
+        <p className="text-neutral-500 max-w-[750px] mx-auto my-2  text-center relative z-10">
+          At Code Biruny, we create a user-friendly platform that connects
+          people and provides effective digital solutions to support businesses
+          and communities.
+        </p>
+        <FeaturesSection />
+      </div>
     </div>
   );
 }
